d10db3f536ecf38abe81812e6d4670d26b1078a3
admin <qikai@playcrab.com>
2024-03-06 21:58:11 +0800

    fmt库增加测试文件

--- 01-fmt标准库/main.go
+++ 01-fmt标准库/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"strings"
 )
 
 type User struct {
@@ -48,6 +51,8 @@ func main() {
 	fmt.Printf("%q\n", s)
 	fmt.Printf("%x\n", s)
 	fmt.Printf("%X\n", s)
+	fmt.Println("---------打印指针----------")
+	fmt.Printf("%p\n", &s)
 	fmt.Println("---------占位符打印----------")
 	s = "我是字符串"
 	fmt.Printf("%  d\n", 10)
@@ -57,10 +62,53 @@ func main() {
 	fmt.Printf("%10.2f\n", 10.14)
 	fmt.Printf("%-10.2f\n", 10.14)
 	fmt.Printf("%010s\n", s)
-	fmt.Println("-------使用Errorf生成panic错误----------")
-	err := fmt.Errorf("用户名格式不正确：%s", "@#￥哈哈")
+
+	fmt.Println("-------Fprint 把内容输出到io输出流中----------")
+	fmt.Fprint(os.Stdout, "标准输出打印\n")
+	fmt.Fprintln(os.Stdout, "标准输出打印")
+	fmt.Fprintf(os.Stdout, "标准输出打印:%v\n", 10)
+
+	fmt.Println("-------Fprint 把内容输出到文件中----------")
+	file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Fprintf(file, "文件输出打印:%v\n", 10)
+	fmt.Fprintln(file, "文件输出打印")
+	fmt.Println("")
+
+	fmt.Println("-------Sprint/f/ln字符串拼接-------------------")
+	fmt.Println(fmt.Sprint("10\n"))
+	fmt.Println(fmt.Sprintln("10"))
+	fmt.Println(fmt.Sprintf("10+10=%d", 20))
+	host := "127.0.0.1"
+	port := 80
+	addr := fmt.Sprintf("%s:%d", host, port)
+	addr2 := host + ":" + fmt.Sprint(port)
+	fmt.Println(addr, addr2)
+
+	fmt.Println("-------使用Errorf生成错误----------")
+	err1 := errors.New("用户名格式不正确")
+	err2 := fmt.Errorf("用户名格式不正确：%s", "@#￥哈哈")
+	if err1 != nil {
+		// panic(err)
+		fmt.Println(err1, err2)
+		fmt.Printf("%T %[1]v", err1)
+	}
+
+	fmt.Println("------------扫描Scan----------------")
+	var (
+		name    string
+		age     int
+		married bool
+	)
+	//从标准输入中读取数据
+	// fmt.Scan(&name, &age, &married)
+	// fmt.Scanln(&name, &age, &married)  //一样的
+	// fmt.Scanf("1:%s 2:%d 3:%t", &name, &age, &married) // 1:张三 2:18 3:true 按照这个顺序进行接收数据
+	//从文件、字符串中读取数据
+	reader := strings.NewReader("1:zhangsan 2:18 3:true")
+	fmt.Fscanf(reader, "1:%s 2:%d 3:%t", &name, &age, &married)
+	fmt.Printf("name:%s,age:%d,married:%t", name, age, married)
 
 }
